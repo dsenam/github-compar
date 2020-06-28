f7749554f5121ba1191f56cbaec68abe0482ac6e
dsenam <dsena.m@hotmail.com>
2020-06-28 19:38:10 -0300

    axios , consumo api, listagem dos dados ok

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,33 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Repository } from './styles';
 
-// api.github.com/repos/
-
-export default function CompareList() {
-    const [arrRepositories, setArrRepositories] = useState([]);
+export default function CompareList({ repositories }) {
+    CompareList.propTypes = {
+        repositories: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number,
+                name: PropTypes.string,
+                owner: PropTypes.shape({
+                    login: PropTypes.string,
+                    avatar_url: PropTypes.string,
+                }),
+                stargazers_count: PropTypes.number,
+                forks_count: PropTypes.number,
+                open_issues_count: PropTypes.number,
+                pushed_at: PropTypes.string,
+            })
+        ).isRequired,
+    };
     return (
         <Container>
-            <Repository>
-                <header>
-                    <img src="" alt="" />
-                    <strong>Teste</strong>
-                    <small>Teste</small>
-                </header>
-                <ul>
-                    <li>
-                        95,019 <small>stars</small>
-                    </li>
-                    <li>
-                        95,019 <small>forks</small>
-                    </li>
-                    <li>
-                        95,019 <small>issues</small>
-                    </li>
-                    <li>
-                        95,019 <small>last commit</small>
-                    </li>
-                </ul>
-            </Repository>
+            {repositories.map((repo) => (
+                <Repository key={repo.id}>
+                    <header>
+                        <img
+                            src={repo.owner.avatar_url}
+                            alt={repo.owner.login}
+                        />
+                        <strong>{repo.name}</strong>
+                        <small>{repo.login}</small>
+                    </header>
+                    <ul>
+                        <li>
+                            {repo.stargazers_count}
+                            <small>stars</small>
+                        </li>
+                        <li>
+                            {repo.forks_count} <small>forks</small>
+                        </li>
+                        <li>
+                            {repo.openopen_issues_count} <small>issues</small>
+                        </li>
+                        <li>
+                            {repo.pushed_at} <small>last commit</small>
+                        </li>
+                    </ul>
+                </Repository>
+            ))}
         </Container>
     );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form } from './styles';
 import CompareList from '../../components/CompareList';
+import api from '../../services/api';
 
 export default function Main() {
+    const [arrRepositories, setArrRepositories] = useState([]);
+    const [repoInput, setRepoInput] = useState('');
+
+    async function handleAddRepository(e) {
+        e.preventDefault();
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            setArrRepositories([...arrRepositories, response.data]);
+            setRepoInput('');
+        } catch (err) {}
+    }
     return (
         <>
             <Container>
                 <h1>GITHUB COMPARE</h1>
-                <Form>
-                    <input type="text" placeholder="usuario/repositório" />
+                <Form onSubmit={handleAddRepository}>
+                    <input
+                        type="text"
+                        placeholder="usuario/repositório"
+                        value={repoInput}
+                        onChange={(e) => setRepoInput(e.target.value)}
+                    />
                     <button type="submit">OK</button>
                 </Form>
             </Container>
-            <CompareList />
+            <CompareList repositories={arrRepositories} />
         </>
     );
 }
